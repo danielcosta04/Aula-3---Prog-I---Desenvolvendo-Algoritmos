82f3e79eb4f3c4a53b9cc8b311042f31ccf7f494
Daniel Costa <124638683+danielcosta04@users.noreply.github.com>
2023-03-23 02:23:06 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(void) {
+//   
+//     a = 30
+//     b = 20
+//     c = a + b // c = 30 + 20 = 50
+//     b = 10
+//     escreva(b= 10, c = 50)
+//     c = 30 + 10 = 40
+//     escreva (30, 10, 40)
+//    ==============================
+//     a = 10
+//     b = a +1 || 10 +1 =11
+//     a = b + 1 || 11 + 1 = 12
+//     b = a + 1 || 12 + 1 = 13
+//     escreva(a) = 12
+//     a = b +1 || 13 + 1 = 14
+//     escreva (a,b || 14,13)
+// 
+  //1. Faça um algoritmo para “Calcular o estoque médio de uma peça”, sendo que estoque médio = (quantidade mínima + quantidade máxima) /2.
+  
+  // float qtdMin = 0;
+  // float qtdMax = 0;
+  // float media = 0;
+
+  // printf("Digite o valor minimo: ");
+  //   scanf("%f", &qtdMin);
+  // printf("Digite o valor maximo: ");
+  //   scanf("%f", &qtdMax);
+  // media = (qtdMin + qtdMax)/2;
+  // printf("A média do estoque é media %.2f", media);
+
+  // 3. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+
+  // int idadeAno = 0;
+  // int idadeMes = 0;
+  // int idadeDias = 0;
+  // int totalDias = 0;
+  // int ano, mes, dia;
+  // printf("Digite os anos da idade: ");
+  //   scanf("%d", &idadeAno);
+  
+  // printf("Digite os meses da idade: ");
+  //   scanf("%d", &idadeMes);
+  
+  // printf("Digite os dias da idade: ");
+  //   scanf("%d", &idadeDias);
+  
+  // ano = idadeAno * 365;
+  // mes = idadeMes * 30;
+  // dia = idadeDias;
+  
+  // totalDias = ano + mes + dia;
+  
+  // printf("O total em dias: %d", totalDias);
+
+  // 4. Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.
+  
+
+
+  
+return 0;
+}
